637dfe6442c5b86ded8095adf4ef22565b3af305
amida <cain.wells@mail.ru>
2024-09-08 13:00:44 +0300

    Managem task repository

--- src/main/java/org/amida/tasktracker/task_api/repository/TaskRepository.java
+++ src/main/java/org/amida/tasktracker/task_api/repository/TaskRepository.java
@@ -4,6 +4,11 @@ import org.amida.tasktracker.task_api.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long userId);
+    Optional<Task> findByUserIdAndTaskId(Long userId, Long taskId);
 }
